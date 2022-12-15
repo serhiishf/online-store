22966b46d975e89a0e360e2dbd03250f2cb59b02
Serhii Sh <unison447@gmail.com>
2022-12-15 18:36:42 +0200

    init: Start project Online-Store

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+//for TypeScript
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
+
+//for JavaScript
+//need add
