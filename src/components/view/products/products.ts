0e7a0da26df6483fc0ec1ac5ddfcfcdd04b72f36
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-12-16 15:10:25 +0100

    feat: add Products render class

--- src/components/view/products/products.ts
+++ src/components/view/products/products.ts
@@ -1,21 +1,36 @@
-//========
-interface Product {
-  id: number,
-  title: string,
-  description: string,
-  price: number,
-  discountPercentage: number,
-  rating: number,
-  stock: number,
-  brand: string,
-  category: string,
-  thumbnail: string,
-  images:Array<string>
-}
-//========
+import { Product } from '../../../types';
 
 export class Products {
-  public draw (data: Product[] | []): void {
-    const fragment = <DocumentFragment>document.createDocumentFragment();
-  }
+    public draw(data: Product[] | []): void {
+        const products = [...data]; // here we can filter data if we need it;
+        const fragment = <DocumentFragment>document.createDocumentFragment();
+        const productItemTemp = <HTMLTemplateElement>document.querySelector('#product-item');
+        const productsList = <HTMLElement>document.querySelector('.products');
+
+        if (products.length > 0) {
+            products.forEach((item: Product) => {
+                const prodClone = <HTMLElement>productItemTemp.content.cloneNode(true);
+
+                (<HTMLElement>prodClone.querySelector('.card__title')).textContent = item.title;
+                (<HTMLElement>prodClone.querySelector('.card__image')).setAttribute(
+                    'src',
+                    item.images[0] || './assets/img/default-image.png'
+                );
+                (<HTMLElement>prodClone.querySelector('.card__image')).setAttribute('alt', item.title);
+                (<HTMLElement>prodClone.querySelector('.card__desc')).textContent = item.description;
+                (<HTMLElement>prodClone.querySelector('.card__stock')).textContent = `In stock: ${item.stock}`;
+                (<HTMLElement>prodClone.querySelector('.card__price')).textContent = `${item.price} $`;
+
+                fragment.append(prodClone);
+            });
+
+            productsList.innerHTML = '';
+            productsList.appendChild(fragment);
+        } else {
+            const p: HTMLParagraphElement = document.createElement('p');
+            p.textContent = 'No results';
+            productsList.innerHTML = '';
+            productsList.after(p);
+        }
+    }
 }
