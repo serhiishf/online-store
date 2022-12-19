52afc840c1e0a3364a49b6113c33cd7e3b0fe1f8
Serhii Sh <unison447@gmail.com>
2022-12-19 12:43:07 +0200

    feat: Create class for load data from API

---
+++ src/components/controller/loader.ts
@@ -0,0 +1,33 @@
+import { Product } from '../../types/index';
+import { UrlApi, JsonProducts } from '../../types/Loader';
+
+export class Loader {
+    rawArr: Product[] = [];
+    urlApi: UrlApi;
+    flag = false;
+
+    constructor(url: UrlApi) {
+        this.urlApi = url;
+    }
+
+    async loadGoods() {
+        this.flag = false;
+        await fetch(`${this.urlApi.base}${this.urlApi.goods}`)
+            .then(this.errorHandler)
+            .then((responce) => responce.json())
+            .then((data: JsonProducts) => {
+                this.rawArr = data.products;
+                this.flag = true;
+                console.log(this.rawArr);
+            });
+    }
+    
+    errorHandler(res: Response) {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404)
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            throw Error(res.statusText);
+        }
+        return res;
+    }
+}
