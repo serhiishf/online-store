bf112541619ac82cc8459576936021d2192c5ddd
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-01-06 22:49:07 +0100

    refactor: delete comments

--- src/pages/app/app.ts
+++ src/pages/app/app.ts
@@ -7,56 +7,40 @@ import MainPage from '../mainPage';
 import ProductPage from '../productPage';
 
 class App {
-    private static container: HTMLElement = <HTMLElement>document.body.querySelector('#app');
+  private static container: HTMLElement = <HTMLElement>document.body.querySelector('#app');
 
-    constructor() {
-        window.addEventListener('load', HeaderController.changeViewOnCartAction);
-        window.addEventListener('hashchange', this.router);
+  constructor() {
+    window.addEventListener('load', HeaderController.changeViewOnCartAction);
+    window.addEventListener('hashchange', this.router);
+  }
+
+  router() {
+    App.renderNewPage(parseRequestUrl());
+  }
+
+  static async renderNewPage(params: UrlParams): Promise<void> {
+    App.container.innerHTML = '';
+    let page: MainPage | ProductPage | CartPage | ErrorPage;
+
+    if (params.page === PagePath.MainPage) {
+      page = new MainPage(params.page);
+    } else if (params.page === PagePath.ProductPage) {
+      page = new ProductPage(params.page, params.id);
+    } else if (params.page === PagePath.CartPage) {
+      page = new CartPage(params.page);
+    } else {
+      page = new ErrorPage(PagePath.ErrorPage, ErrorTypes.Error_404);
     }
 
-    router() {
-        App.renderNewPage(parseRequestUrl());
+    if (page) {
+      const pageHTML = await page.render();
+      App.container.append(pageHTML);
     }
+  }
 
-    static async renderNewPage(params: UrlParams): Promise<void> {
-        App.container.innerHTML = '';
-        let page: MainPage | ProductPage | CartPage | ErrorPage;
-        // const url = new URL(window.location.href);
-        if (params.page === PagePath.MainPage) {
-            // url.searchParams.forEach((_, key, parent) => {
-            //     if (key === 'id' || key === 'limit' || key === 'page') {
-            //         parent.delete(key);
-            //     }
-            // });
-            // window.history.pushState(null, '', url.toString());
-            page = new MainPage(params.page);
-        } else if (params.page === PagePath.ProductPage) {
-            // url.searchParams.forEach((_, key, parent) => {
-            //     if (key !== 'id') {
-            //         parent.delete(key);
-            //     }
-            // });
-            // window.history.pushState(null, '', url.toString());
-            page = new ProductPage(params.page, params.id);
-        } else if (params.page === PagePath.CartPage) {
-            // url.searchParams.forEach((_, key, parent) => {
-            //     parent.delete(key);
-            // });
-            // window.history.pushState(null, '', url.toString());
-            page = new CartPage(params.page);
-        } else {
-            page = new ErrorPage(PagePath.ErrorPage, ErrorTypes.Error_404);
-        }
-
-        if (page) {
-            const pageHTML = await page.render();
-            App.container.append(pageHTML);
-        }
-    }
-
-    run() {
-        this.router();
-    }
+  run() {
+    this.router();
+  }
 }
 
 export default App;
