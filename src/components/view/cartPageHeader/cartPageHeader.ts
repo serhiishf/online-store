780de0b67951f6175b53a3fc57e124d8188eab02
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-01-08 00:02:22 +0100

    feat: add pagination

--- src/components/view/cartPageHeader/cartPageHeader.ts
+++ src/components/view/cartPageHeader/cartPageHeader.ts
@@ -3,104 +3,113 @@ import { CartController } from '../../controller/cartController';
 import { parseRequestUrl } from '../../controller/parseRequestUrl';
 
 export class CartPageHeader {
-    headerEl: HTMLElement;
-    cart: Cart | null;
-    urlParams: UrlParams;
-    lastPage: number;
+  headerEl: HTMLElement;
+  cart: Cart | null;
+  urlParams: UrlParams;
+  lastPage: number;
 
-    constructor() {
-        this.headerEl = document.createElement('div');
-        this.headerEl.classList.add('cart__header-thumb');
-        this.cart = CartController.getCart() ? JSON.parse(<string>CartController.getCart()) : null;
-        this.urlParams = parseRequestUrl();
-        const lastPageMath = (): number => {
-            const oneTypeProductCount: number = this.cart?.oneTypeProductCount || 1;
-            const limitOnPage = Number(this.urlParams.search.limit) || 2; //like in html templ
-            return Math.ceil(oneTypeProductCount / limitOnPage);
-        };
-        this.lastPage = lastPageMath();
+  constructor() {
+    this.headerEl = document.createElement('div');
+    this.headerEl.classList.add('cart__header-thumb');
+    this.cart = CartController.getCart() ? JSON.parse(<string>CartController.getCart()) : null;
+    this.urlParams = parseRequestUrl();
+    const lastPageMath = (): number => {
+      const oneTypeProductCount: number = this.cart?.oneTypeProductCount || 1;
+      const limitOnPage = Number(this.urlParams.search.limit) || 2; //like in html templ
+      return Math.ceil(oneTypeProductCount / limitOnPage);
+    };
+    this.lastPage = lastPageMath();
+  }
+
+  private setPageQueryParams(pageCountEl: HTMLElement) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', <string>pageCountEl.textContent);
+    window.history.pushState(null, '', url.toString());
+  }
+  private setLimitQueryParams(inputLimitEl: HTMLInputElement) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('limit', `${inputLimitEl.value}`);
+    window.history.pushState(null, '', url.toString());
+  }
+  private setActualLastPage(): number {
+    const inputLimitEl = <HTMLInputElement>document.querySelector('#limit-prod-on-page');
+    const cart = CartController.getCart() ? JSON.parse(<string>CartController.getCart()) : null;
+    const oneTypeProductCount: number = cart?.oneTypeProductCount || 1;
+    return Math.ceil(oneTypeProductCount / Number(inputLimitEl.value));
+  }
+
+  private onPrevPageBtnClick(prevBtnEl: HTMLButtonElement, pageCountEl: HTMLElement, nextBtnEl: HTMLButtonElement) {
+    nextBtnEl.disabled = false;
+    pageCountEl.textContent = `${+(<string>pageCountEl.textContent) - 1}`;
+
+    this.setPageQueryParams(pageCountEl);
+    //?
+    const lastPage = this.setActualLastPage();
+    if (pageCountEl.textContent === lastPage.toString()) {
+      nextBtnEl.disabled = true;
+    }
+    //?
+    if (pageCountEl.textContent === '1') {
+      prevBtnEl.disabled = true;
+    }
+  }
+
+  private onNextPageBtnClick(prevBtnEl: HTMLButtonElement, pageCountEl: HTMLElement, nextBtnEl: HTMLButtonElement) {
+    prevBtnEl.disabled = false;
+    pageCountEl.textContent = `${+(<string>pageCountEl.textContent) + 1}`;
+    this.setPageQueryParams(pageCountEl);
+    const lastPage = this.setActualLastPage();
+    if (pageCountEl.textContent === lastPage.toString()) {
+      nextBtnEl.disabled = true;
+    }
+  }
+
+  private onLimitChange(inputLimitEl: HTMLInputElement, pageCountEl: HTMLElement, nextBtnEl: HTMLButtonElement) {
+    this.setLimitQueryParams(inputLimitEl);
+    const lastPage = this.setActualLastPage();
+    if (pageCountEl.textContent === lastPage.toString()) {
+      nextBtnEl.disabled = true;
+      pageCountEl.textContent = lastPage.toString();
+      this.setPageQueryParams(pageCountEl);
+    } else {
+      nextBtnEl.disabled = false;
+    }
+  }
+
+  public draw() {
+    const fragment = <DocumentFragment>document.createDocumentFragment();
+    const template = <HTMLTemplateElement>document.querySelector('#cart-page-header');
+
+    const prodClone = <HTMLElement>template.content.cloneNode(true);
+    const inputLimitEl = <HTMLInputElement>prodClone.querySelector('#limit-prod-on-page');
+    const prevPageButtonEl = <HTMLButtonElement>prodClone.querySelector('.cart__sub-page-btn-prev');
+    const nextPageButtonEl = <HTMLButtonElement>prodClone.querySelector('.cart__sub-page-btn-next');
+    const pageNumEl = <HTMLElement>prodClone.querySelector('.cart__sub-page-count');
+    //set limit actual value:
+    if (this.urlParams.search.limit) {
+      inputLimitEl.value = this.urlParams.search.limit;
     }
 
-    private setPageQueryParams(pageCountEl: HTMLElement) {
-        const url = new URL(window.location.href);
-        url.searchParams.set('page', <string>pageCountEl.textContent);
-        window.history.pushState(null, '', url.toString());
+    inputLimitEl.max = this.cart?.oneTypeProductCount.toString() || '2';
+    inputLimitEl.addEventListener('change', () => this.onLimitChange(inputLimitEl, pageNumEl, nextPageButtonEl));
+
+    //get query params for render  actual page
+    pageNumEl.textContent = this.urlParams.search.page ? this.urlParams.search.page.toString() : '1';
+    if (pageNumEl.textContent === '1') {
+      prevPageButtonEl.disabled = true;
     }
-
-    private onPrevPageBtnClick(prevBtnEl: HTMLButtonElement, pageCountEl: HTMLElement, nextBtnEl: HTMLButtonElement) {
-        nextBtnEl.disabled = false;
-        pageCountEl.textContent = `${+(<string>pageCountEl.textContent) - 1}`;
-
-        this.setPageQueryParams(pageCountEl);
-
-        //rerender itims
-        if (pageCountEl.textContent === '1') {
-            prevBtnEl.disabled = true;
-        }
+    if (this.lastPage <= Number(pageNumEl.textContent)) {
+      nextPageButtonEl.disabled = true;
     }
+    prevPageButtonEl.addEventListener('click', () =>
+      this.onPrevPageBtnClick(prevPageButtonEl, pageNumEl, nextPageButtonEl)
+    );
+    nextPageButtonEl.addEventListener('click', () =>
+      this.onNextPageBtnClick(prevPageButtonEl, pageNumEl, nextPageButtonEl)
+    );
 
-    private onNextPageBtnClick(prevBtnEl: HTMLButtonElement, pageCountEl: HTMLElement, nextBtnEl: HTMLButtonElement) {
-        prevBtnEl.disabled = false;
-        pageCountEl.textContent = `${+(<string>pageCountEl.textContent) + 1}`;
-
-        this.setPageQueryParams(pageCountEl);
-        //rerender itims
-        if (this.lastPage <= Number(pageCountEl.textContent)) {
-            nextBtnEl.disabled = true;
-        }
-    }
-
-    private onLimitChange(inputLimitEl: HTMLInputElement, pageCountEl: HTMLElement, nextBtnEl: HTMLButtonElement) {
-        const url = new URL(window.location.href);
-        url.searchParams.set('limit', `${inputLimitEl.value}`);
-        window.history.pushState(null, '', url.toString());
-
-        this.lastPage = Math.ceil(<number>this.cart?.oneTypeProductCount / Number(inputLimitEl.value));
-        if (this.lastPage <= Number(pageCountEl.textContent)) {
-            nextBtnEl.disabled = true;
-            pageCountEl.textContent = this.lastPage.toString();
-            this.setPageQueryParams(pageCountEl);
-        } else {
-            nextBtnEl.disabled = false;
-        }
-    }
-
-    public draw() {
-        const fragment = <DocumentFragment>document.createDocumentFragment();
-        const template = <HTMLTemplateElement>document.querySelector('#cart-page-header');
-
-        const prodClone = <HTMLElement>template.content.cloneNode(true);
-        const inputLimitEl = <HTMLInputElement>prodClone.querySelector('#limit-prod-on-page');
-        const prevPageButtonEl = <HTMLButtonElement>prodClone.querySelector('.cart__sub-page-btn-prev');
-        const nextPageButtonEl = <HTMLButtonElement>prodClone.querySelector('.cart__sub-page-btn-next');
-        const pageNumEl = <HTMLElement>prodClone.querySelector('.cart__sub-page-count');
-        //set limit actual value:
-        if (this.urlParams.search.limit) {
-            inputLimitEl.value = this.urlParams.search.limit;
-        }
-        // if (this.cart && this.cart.oneTypeProductCount > 5) {
-        //set max limit products on page:
-        inputLimitEl.max = this.cart?.oneTypeProductCount.toString() || '2';
-        // }
-        inputLimitEl.addEventListener('change', () => this.onLimitChange(inputLimitEl, pageNumEl, nextPageButtonEl));
-
-        //get query params for render  actual page
-        pageNumEl.textContent = this.urlParams.search.page ? this.urlParams.search.page.toString() : '1';
-        if (pageNumEl.textContent === '1') {
-            prevPageButtonEl.disabled = true;
-        }
-        if (this.lastPage <= Number(pageNumEl.textContent)) {
-            nextPageButtonEl.disabled = true;
-        }
-        prevPageButtonEl.addEventListener('click', () =>
-            this.onPrevPageBtnClick(prevPageButtonEl, pageNumEl, nextPageButtonEl)
-        );
-        nextPageButtonEl.addEventListener('click', () =>
-            this.onNextPageBtnClick(prevPageButtonEl, pageNumEl, nextPageButtonEl)
-        );
-
-        fragment.append(prodClone);
-        this.headerEl.append(fragment);
-        return this.headerEl;
-    }
+    fragment.append(prodClone);
+    this.headerEl.append(fragment);
+    return this.headerEl;
+  }
 }
--- src/components/view/productsInCart/productsInCart.ts
+++ src/components/view/productsInCart/productsInCart.ts
@@ -2,131 +2,204 @@ import { Cart, Product, ProductInCart as IProductInCart } from '../../../types';
 import { CartController } from '../../controller/cartController';
 import { HeaderController } from '../../controller/headerController';
 import { LoaderSingleProduct } from '../../controller/loaderSingleProduct';
+import { parseRequestUrl } from '../../controller/parseRequestUrl';
 import { CartPageHeader } from '../cartPageHeader';
 
+interface PagePageParams {
+  page: number;
+  limit: number;
+}
+
 export class ProductsInCart {
-    protected items: IProductInCart[];
-    protected listEl: HTMLUListElement;
-    thumb: HTMLElement;
-    headerEl: HTMLElement;
+  protected items: IProductInCart[];
+  protected listEl: HTMLUListElement;
+  thumb: HTMLElement;
+  headerEl: HTMLElement;
 
-    constructor(storageCart: Cart) {
-        this.items = storageCart.products;
-        this.thumb = document.createElement('div');
-        this.thumb.classList.add('cart__sub-thumb');
+  constructor(storageCart: Cart) {
+    this.items = storageCart.products;
+    this.thumb = document.createElement('div');
+    this.thumb.classList.add('cart__sub-thumb');
 
-        this.listEl = document.createElement('ol');
-        this.listEl.classList.add('cart__product-list');
-        this.headerEl = new CartPageHeader().draw();
+    this.listEl = document.createElement('ol');
+    this.listEl.classList.add('cart__product-list');
+    this.headerEl = new CartPageHeader().draw();
+  }
+
+  private async getProducts(): Promise<Product[] | undefined> {
+    try {
+      const promises = this.items.map((el: IProductInCart) => LoaderSingleProduct.fetchProduct(el.id.toString()));
+      const products: Product[] = await Promise.all(promises);
+      return products;
+    } catch (error) {
+      let message: string = 'Restart the page!';
+      if (typeof error === 'string') {
+        message = error;
+      } else if (error instanceof Error) {
+        message = error.message;
+      }
+      console.log(`Something went wrong! ${message}`);
     }
+  }
 
-    private async getProducts(): Promise<Product[] | undefined> {
-        try {
-            const promises = this.items.map((el: IProductInCart) => LoaderSingleProduct.fetchProduct(el.id.toString()));
-            const products: Product[] = await Promise.all(promises);
-            return products;
-        } catch (error) {
-            let message: string = 'Restart the page!';
-            if (typeof error === 'string') {
-                message = error;
-                error;
-            } else if (error instanceof Error) {
-                message = error.message;
-            }
-            console.log(`Something went wrong! ${message}`);
+  private addOrRemoveProdAction(parentEl: HTMLElement, prodId: number, prodPrice: number, inStock: number) {
+    const incrBtnEl = <HTMLButtonElement>parentEl.querySelector('.cart__product-count-incr');
+    const decrBtnEl = <HTMLButtonElement>parentEl.querySelector('.cart__product-count-decr');
+    const countEl = <HTMLElement>parentEl.querySelector('.cart__product-count');
+    const priceEl = <HTMLElement>parentEl.querySelector('.cart__product-price-quantity');
+
+    const removeProduct = (): void => {
+      const pageCount = <HTMLElement>this.headerEl.querySelector('.cart__sub-page-count');
+      const prodQuantity = 1;
+
+      incrBtnEl.disabled = false;
+      CartController.removeOneProductOneType(prodId, prodPrice, prodQuantity);
+      HeaderController.changeViewOnCartAction();
+
+      countEl.textContent = `${Number(<string>countEl.textContent) - prodQuantity}`;
+      priceEl.textContent = `${Number(<string>priceEl.textContent) - prodPrice * prodQuantity}`;
+
+      //remove product item from list:
+      if (countEl.textContent === '0') {
+        const productItemEl = <HTMLElement>document.querySelector(`#product-id-${prodId}`);
+        productItemEl.remove();
+        ProductsInCart.onPaginationChange();
+
+        const allProducts = document.querySelectorAll('.cart__product-item');
+        const hiddenProducts = document.querySelectorAll('.cart__product-item.hidden');
+        //change actual page if it is last page:
+        const urlParams = parseRequestUrl();
+        const cart = CartController.getCart() ? JSON.parse(<string>CartController.getCart()) : null;
+        const oneTypeProductCount: number = cart?.oneTypeProductCount || 1;
+        const limitOnPage = Number(urlParams.search.limit) || 2;
+        const lastPage = Math.ceil(oneTypeProductCount / limitOnPage);
+        const isLastPage = Number(pageCount.textContent) >= lastPage;
+        if (isLastPage) {
+          (<HTMLButtonElement>this.headerEl.querySelector('.cart__sub-page-btn-next')).disabled = true;
         }
-    }
-
-    private addOrRemoveProdAction(parentEl: HTMLElement, prodId: number, prodPrice: number, inStock: number) {
-        const incrBtnEl = <HTMLButtonElement>parentEl.querySelector('.cart__product-count-incr');
-        const decrBtnEl = <HTMLButtonElement>parentEl.querySelector('.cart__product-count-decr');
-        const countEl = <HTMLElement>parentEl.querySelector('.cart__product-count');
-        const priceEl = <HTMLElement>parentEl.querySelector('.cart__product-price-quantity');
-
-        const removeProduct = (): void => {
-            const prodQuantity = 1;
-            CartController.removeOneProductOneType(prodId, prodPrice, prodQuantity);
-            HeaderController.changeViewOnCartAction();
-            incrBtnEl.disabled = false;
-
-            countEl.textContent = `${Number(<string>countEl.textContent) - prodQuantity}`;
-            priceEl.textContent = `${Number(<string>priceEl.textContent) - prodPrice * prodQuantity}`;
-            if (countEl.textContent === '0') {
-                const parentElInDOM = <HTMLElement>document.querySelector(`#product-id-${prodId}`);
-                parentElInDOM.remove();
-            }
-            const cartStorage = CartController.getCart();
-            if (cartStorage) {
-                const cart: Cart = JSON.parse(cartStorage);
-                if (cart.totalCount === 0) {
-                    const alternativeTxt = document.createElement('p');
-                    alternativeTxt.classList.add('alternative-txt');
-                    alternativeTxt.textContent = 'Your Cart is Empty';
-                    this.thumb.innerHTML = '';
-                    this.thumb.append(alternativeTxt);
-                }
-            }
-        };
-
-        const addProduct = (): void => {
-            const prodQuantity = 1;
-            CartController.addProduct(prodId, prodPrice, prodQuantity);
-            HeaderController.changeViewOnCartAction();
-
-            countEl.textContent = `${Number(<string>countEl.textContent) + prodQuantity}`;
-            priceEl.textContent = `${Number(<string>priceEl.textContent) + prodPrice * prodQuantity}`;
-
-            if (Number(countEl.textContent) === inStock) {
-                incrBtnEl.disabled = true;
-            }
-        };
-
-        incrBtnEl.addEventListener('click', addProduct);
-        decrBtnEl.addEventListener('click', removeProduct);
-    }
-
-    public async draw() {
-        const products = await this.getProducts();
-        if (products) {
-            const fragment = <DocumentFragment>document.createDocumentFragment();
-            const template = <HTMLTemplateElement>document.querySelector('#product-in-cart');
-
-            products.forEach((prod) => {
-                const prodClone = <HTMLElement>template.content.cloneNode(true);
-                (<HTMLImageElement>prodClone.querySelector('.cart__product-item')).setAttribute(
-                    'id',
-                    `product-id-${prod.id}`
-                );
-                //product image:
-                const imgEl = <HTMLImageElement>prodClone.querySelector('.cart__product-img');
-                imgEl.setAttribute('src', prod.thumbnail);
-                imgEl.setAttribute('alt', prod.title);
-                //product info:
-                (<HTMLElement>prodClone.querySelector('.cart__product-title')).textContent = prod.title;
-                (<HTMLElement>prodClone.querySelector('.cart__product-desc')).textContent = prod.description;
-                (<HTMLElement>prodClone.querySelector('.cart__product-inf--rating')).textContent =
-                    prod.rating.toString();
-                (<HTMLElement>prodClone.querySelector('.cart__product-inf--discount')).textContent =
-                    prod.discountPercentage.toString();
-                (<HTMLElement>prodClone.querySelector('.cart__inf--stock')).textContent = prod.stock.toString();
-                //how many in cart:
-                const count: number | undefined = this.items.find((el) => el.id === prod.id)?.count;
-                const countEl = <HTMLElement>prodClone.querySelector('.cart__product-count');
-                countEl.textContent = count ? count.toString() : '1';
-                //total price for one product*product quantity:
-                const priceEl = <HTMLElement>prodClone.querySelector('.cart__product-price-quantity');
-                priceEl.textContent = count ? `${prod.price * count}` : `${prod.price}`;
-
-                // + and - product in Cart
-                this.addOrRemoveProdAction(prodClone, prod.id, prod.price, prod.stock);
-
-                fragment.append(prodClone);
-            });
-
-            this.listEl.innerHTML = '';
-            this.listEl.append(fragment);
+        if (allProducts.length === hiddenProducts.length && isLastPage) {
+          (<HTMLButtonElement>this.headerEl.querySelector('.cart__sub-page-btn-prev')).click();
         }
-        this.thumb.append(this.headerEl, this.listEl);
-        return this.thumb;
+      }
+
+      //clear cart page on empty cart:
+      const cartStorage = CartController.getCart();
+      if (cartStorage) {
+        const cart: Cart = JSON.parse(cartStorage);
+        if (cart.totalCount === 0) {
+          const alternativeTxt = document.createElement('p');
+          alternativeTxt.classList.add('alternative-txt');
+          alternativeTxt.textContent = 'Your Cart is Empty';
+          this.thumb.innerHTML = '';
+          this.thumb.append(alternativeTxt);
+        }
+      }
+    };
+
+    const addProduct = (): void => {
+      const prodQuantity = 1;
+      CartController.addProduct(prodId, prodPrice, prodQuantity);
+      HeaderController.changeViewOnCartAction();
+
+      countEl.textContent = `${Number(<string>countEl.textContent) + prodQuantity}`;
+      priceEl.textContent = `${Number(<string>priceEl.textContent) + prodPrice * prodQuantity}`;
+
+      if (Number(countEl.textContent) === inStock) {
+        incrBtnEl.disabled = true;
+      }
+    };
+
+    incrBtnEl.addEventListener('click', addProduct);
+    decrBtnEl.addEventListener('click', removeProduct);
+  }
+
+  private static showPageAndLimit(): PagePageParams {
+    const URLparamsObj = parseRequestUrl();
+
+    const page: PagePageParams['page'] = Number(URLparamsObj.search.page) || 1; //page from query params or first page
+    const limit: PagePageParams['limit'] = Number(URLparamsObj.search.limit) || 2; //limit from query params or min 2 prod on page
+
+    return {
+      page,
+      limit,
+    };
+  }
+
+  private static startEndIndexOnPage(page: number, limit: number) {
+    const firstIndex = (page - 1) * limit;
+    const lastIndex = firstIndex + (limit - 1);
+    return [firstIndex, lastIndex];
+  }
+
+  private static hideRedundantProduct(prodIndex: number, prodEl: HTMLElement) {
+    const { page, limit } = ProductsInCart.showPageAndLimit();
+    const [start, end] = ProductsInCart.startEndIndexOnPage(page, limit);
+    if (end < prodIndex || prodIndex < start) {
+      prodEl.classList.add('hidden');
     }
+  }
+
+  private static onPaginationChange() {
+    const prodElCollection = document.querySelectorAll<HTMLElement>(`.cart__product-item`);
+    prodElCollection.forEach((el) => el.classList.remove('hidden'));
+    prodElCollection.forEach((el, i) => {
+      ProductsInCart.hideRedundantProduct(i, el);
+    });
+  }
+
+  public async draw() {
+    //limitinput action
+    const limitInputElem = <HTMLInputElement>this.headerEl.querySelector('#limit-prod-on-page');
+    limitInputElem.addEventListener('change', ProductsInCart.onPaginationChange);
+    //prev btn el action
+    const prevPageBtnEl = <HTMLButtonElement>this.headerEl.querySelector('.cart__sub-page-btn-prev');
+    prevPageBtnEl.addEventListener('click', ProductsInCart.onPaginationChange);
+
+    const nextPageBtnEl = <HTMLButtonElement>this.headerEl.querySelector('.cart__sub-page-btn-next');
+    nextPageBtnEl.addEventListener('click', ProductsInCart.onPaginationChange);
+    //===================================================
+    const products = await this.getProducts();
+
+    if (products) {
+      const fragment = <DocumentFragment>document.createDocumentFragment();
+      const template = <HTMLTemplateElement>document.querySelector('#product-in-cart');
+
+      products.forEach((prod, index) => {
+        const prodClone = <HTMLElement>template.content.cloneNode(true);
+
+        const prodEl = <HTMLElement>prodClone.querySelector('.cart__product-item');
+        ProductsInCart.hideRedundantProduct(index, prodEl);
+        prodEl.setAttribute('id', `product-id-${prod.id}`);
+
+        //product image:
+        const imgEl = <HTMLImageElement>prodClone.querySelector('.cart__product-img');
+        imgEl.setAttribute('src', prod.thumbnail);
+        imgEl.setAttribute('alt', prod.title);
+        //product info:
+        (<HTMLElement>prodClone.querySelector('.cart__product-title')).textContent = prod.title;
+        (<HTMLElement>prodClone.querySelector('.cart__product-desc')).textContent = prod.description;
+        (<HTMLElement>prodClone.querySelector('.cart__product-inf--rating')).textContent = prod.rating.toString();
+        (<HTMLElement>prodClone.querySelector('.cart__product-inf--discount')).textContent =
+          prod.discountPercentage.toString();
+        (<HTMLElement>prodClone.querySelector('.cart__inf--stock')).textContent = prod.stock.toString();
+        //how many in cart:
+        const count: number | undefined = this.items.find((el) => el.id === prod.id)?.count;
+        const countEl = <HTMLElement>prodClone.querySelector('.cart__product-count');
+        countEl.textContent = count ? count.toString() : '1';
+        //total price for one product*product quantity:
+        const priceEl = <HTMLElement>prodClone.querySelector('.cart__product-price-quantity');
+        priceEl.textContent = count ? `${prod.price * count}` : `${prod.price}`;
+
+        // + and - product in Cart
+        this.addOrRemoveProdAction(prodClone, prod.id, prod.price, prod.stock);
+
+        fragment.append(prodClone);
+      });
+
+      this.listEl.innerHTML = '';
+      this.listEl.append(fragment);
+    }
+    this.thumb.append(this.headerEl, this.listEl);
+    return this.thumb;
+  }
 }
